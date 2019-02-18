cdc45d02099586bf7fc8380855bebca5d003ba8a
Atul Dwivedi <atul16dwivedi@gmail.com>
2019-02-18 21:47:17 +0530

    correcting the spelling

--- learn-kotlin/src/main/kotlin/com/keykotlin/learn/functions/DefaultArgumentsFunctions.kt
+++ learn-kotlin/src/main/kotlin/com/keykotlin/learn/functions/DefaultArgumentsFunctions.kt
@@ -14,18 +14,18 @@ fun sumAndDoSomeThing(a: Int = 10, b: Int = 20, action: () -> Unit) {
 }
 
 open class A {
-    open fun doSomthing(a: Int = 10) {
+    open fun doSomething(a: Int = 10) {
         println("A: $a")
     }
 }
 
 class B : A() {
     //an overriding functions is not allowed to specify default values for its parameters
-    /*override fun doSomthing(a: Int = 12) {
+    /*override fun doSomething(a: Int = 12) {
         println("B: $a")
     }*/
 
-    override fun doSomthing(a: Int) {
+    override fun doSomething(a: Int) {
         println("B: $a")
     }
 }
@@ -38,6 +38,6 @@ fun main(args: Array<String>) {
     sumAndDoSomeThing(1) { print("Hello, ") }
     sumAndDoSomeThing(b = 1) { print("Hello, ") }
     sumAndDoSomeThing(1, 2) { print("Hello, ") }
-    B().doSomthing() // B: 10
-    B().doSomthing(20) // B: 20
+    B().doSomething() // B: 10
+    B().doSomething(20) // B: 20
 }
